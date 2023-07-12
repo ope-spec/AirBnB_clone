203df6ae1f658e4556a5348df99a56d44f527bf7
KimberlyPeters <kimberlypeters468@gmail.com>
2023-07-12 10:32:13 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,15 @@ class BaseModel:
     Defines common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Variable length keyword argument list.
+                      Each key-value pair represents
+                      an attribute-value assignment.
 
         Attributes:
             id (str): Unique identifier assigned to the instance.
@@ -24,9 +30,18 @@ class BaseModel:
             updated_at (datetime): Date and time when the instance
                                    is last updated.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f'
+                        )
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -54,11 +69,11 @@ class BaseModel:
         Returns a dictionary representation of the BaseModel instance.
 
         Returns:
-            dict: Dictionary containing all keys/values
-                  of __dict__ of the instance.
+            dict: Dictionary containing all keys/values of
+                  __dict__ of the instance.
                   Includes the __class__ key with the class name.
-                  The created_at and updated_at attributes are
-                  converted to string objects in ISO format.
+                  The created_at and updated_at attributes are converted
+                  to string objects in ISO format.
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
