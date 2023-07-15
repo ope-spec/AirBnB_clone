c13da296db3ce2da93158399cce921f67e0542af
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-07-15 13:00:23 +0100

    Updated Console

--- console.py
+++ console.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Module: console
-Entry point for the HBNB command interpreter.
-"""
+"""This module defines the HBNBCommand class, a command interpreter."""
 
 import cmd
+import json
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +23,119 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Print the string representation of an instance."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in storage.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key in storage.all():
+            print(storage.all()[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in storage.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key in storage.all():
+            del storage.all()[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """Print string representations of all instances."""
+        args = arg.split()
+        if args and args[0] not in storage.classes:
+            print("** class doesn't exist **")
+            return
+
+        instances = []
+        for key in storage.all():
+            if not args or storage.all()[key].__class__.__name__ == args[0]:
+                instances.append(str(storage.all()[key]))
+        print(instances)
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in storage.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key in storage.all():
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            if len(args) < 4:
+                print("** value missing **")
+                return
+
+            attribute_name = args[2]
+            attribute_value = args[3]
+
+            instance = storage.all()[key]
+            attribute_type = type(getattr(instance, attribute_name, None))
+            if attribute_type is None:
+                print("** attribute doesn't exist **")
+                return
+
+            setattr(instance, attribute_name, attribute_type(attribute_value))
+            instance.save()
+        else:
+            print("** no instance found **")
 
 
 if __name__ == '__main__':
